8a6dec28a0affdf7eaf455a43be8db862fd9aa09
Branko Djordjevic <brankodj89@gmail.com>
2018-10-17 19:48:34 +0200

    Added descriptions and changed types for fields

--- scripts/process.py
+++ scripts/process.py
@@ -6,40 +6,40 @@ import urllib.request
 base_url = 'https://fred.stlouisfed.org/data/'
 inverse = ['Australia', 'Euro', 'Ireland', 'New Zealand', 'United Kingdom']
 country_codes = {
-    'Australia': {'daily': 'DEXUSAL', 'monthly': 'EXUSAL', 'yearly': 'AEXUSAL'},
-    'Austria': {'daily': '', 'monthly': 'EXAUUS', 'yearly': ''},
-    'Belgium': {'daily': '', 'monthly': 'EXBEUS', 'yearly': ''},
-    'Brazil': {'daily': 'DEXBZUS', 'monthly': 'EXBZUS', 'yearly': 'AEXBZUS'},
-    'Canada': {'daily': 'DEXCAUS', 'monthly': 'EXCAUS', 'yearly': 'AEXCAUS'},
-    'China': {'daily': 'DEXCHUS', 'monthly': 'EXCHUS', 'yearly': 'AEXCHUS'},
-    'Denmark': {'daily': 'DEXDNUS', 'monthly': 'EXDNUS', 'yearly': 'AEXDNUS'},
-    'Euro': {'daily': 'DEXUSEU', 'monthly': 'EXUSEU', 'yearly': 'AEXUSEU'},
-    'Finland': {'daily': '', 'monthly': 'EXFNUS', 'yearly': ''},
-    'France': {'daily': '', 'monthly': 'EXFRUS', 'yearly': ''},
-    'Germany': {'daily': '', 'monthly': 'EXGEUS', 'yearly': ''},
-    'Greece': {'daily': '', 'monthly': 'EXGRUS', 'yearly': ''},
-    'Hong Kong': {'daily': 'DEXHKUS', 'monthly': 'EXHKUS', 'yearly': 'AEXHKUS'},
-    'India': {'daily': 'DEXINUS', 'monthly': 'EXINUS', 'yearly': 'AEXINUS'},
-    'Ireland': {'daily': '', 'monthly': 'EXUSIR', 'yearly': ''},
-    'Italy': {'daily': '', 'monthly': 'EXITUS', 'yearly': ''},
-    'Japan': {'daily': 'DEXJPUS', 'monthly': 'EXJPUS', 'yearly': 'AEXJPUS'},
-    'Malaysia': {'daily': 'DEXMAUS', 'monthly': 'EXMAUS', 'yearly': 'AEXMAUS'},
-    'Mexico': {'daily': 'DEXMXUS', 'monthly': 'EXMXUS', 'yearly': 'AEXMXUS'},
-    'Netherlands': {'daily': '', 'monthly': 'EXNEUS', 'yearly': ''},
-    'New Zealand': {'daily': 'DEXUSNZ', 'monthly': 'EXUSNZ', 'yearly': 'AEXUSNZ'},
-    'Norway': {'daily': 'DEXNOUS', 'monthly': 'EXNOUS', 'yearly': 'AEXNOUS'},
-    'Portugal': {'daily': '', 'monthly': 'EXPOUS', 'yearly': ''},
-    'Singapore': {'daily': 'DEXSIUS', 'monthly': 'EXSIUS', 'yearly': 'AEXSIUS'},
-    'South Africa': {'daily': 'DEXSFUS', 'monthly': 'EXSFUS', 'yearly': 'AEXSFUS'},
-    'South Korea': {'daily': 'DEXKOUS', 'monthly': 'EXKOUS', 'yearly': 'AEXKOUS'},
-    'Spain': {'daily': '', 'monthly': 'EXSPUS', 'yearly': ''},
-    'Sri Lanka': {'daily': '', 'monthly': 'EXSLUS', 'yearly': ''},
-    'Sweden': {'daily': 'DEXSDUS', 'monthly': 'EXSDUS', 'yearly': 'AEXSDUS'},
-    'Switzerland': {'daily': 'DEXSZUS', 'monthly': 'EXSZUS', 'yearly': 'AEXSZUS'},
-    'Taiwan': {'daily': 'DEXTAUS', 'monthly': 'EXTAUS', 'yearly': 'AEXTAUS'},
-    'Thailand': {'daily': 'DEXTHUS', 'monthly': 'EXTHUS', 'yearly': 'AEXTHUS'},
-    'United Kingdom': {'daily': 'DEXUSUK', 'monthly': 'EXUSUK', 'yearly': ''},
-    'Venezuela': {'daily': 'DEXVZUS', 'monthly': 'EXVZUS', 'yearly': 'AEXVZUS'}
+    'Australia': {'daily': 'DEXUSAL', 'monthly': 'EXUSAL', 'annual': 'AEXUSAL'},
+    'Austria': {'daily': '', 'monthly': 'EXAUUS', 'annual': ''},
+    'Belgium': {'daily': '', 'monthly': 'EXBEUS', 'annual': ''},
+    'Brazil': {'daily': 'DEXBZUS', 'monthly': 'EXBZUS', 'annual': 'AEXBZUS'},
+    'Canada': {'daily': 'DEXCAUS', 'monthly': 'EXCAUS', 'annual': 'AEXCAUS'},
+    'China': {'daily': 'DEXCHUS', 'monthly': 'EXCHUS', 'annual': 'AEXCHUS'},
+    'Denmark': {'daily': 'DEXDNUS', 'monthly': 'EXDNUS', 'annual': 'AEXDNUS'},
+    'Euro': {'daily': 'DEXUSEU', 'monthly': 'EXUSEU', 'annual': 'AEXUSEU'},
+    'Finland': {'daily': '', 'monthly': 'EXFNUS', 'annual': ''},
+    'France': {'daily': '', 'monthly': 'EXFRUS', 'annual': ''},
+    'Germany': {'daily': '', 'monthly': 'EXGEUS', 'annual': ''},
+    'Greece': {'daily': '', 'monthly': 'EXGRUS', 'annual': ''},
+    'Hong Kong': {'daily': 'DEXHKUS', 'monthly': 'EXHKUS', 'annual': 'AEXHKUS'},
+    'India': {'daily': 'DEXINUS', 'monthly': 'EXINUS', 'annual': 'AEXINUS'},
+    'Ireland': {'daily': '', 'monthly': 'EXUSIR', 'annual': ''},
+    'Italy': {'daily': '', 'monthly': 'EXITUS', 'annual': ''},
+    'Japan': {'daily': 'DEXJPUS', 'monthly': 'EXJPUS', 'annual': 'AEXJPUS'},
+    'Malaysia': {'daily': 'DEXMAUS', 'monthly': 'EXMAUS', 'annual': 'AEXMAUS'},
+    'Mexico': {'daily': 'DEXMXUS', 'monthly': 'EXMXUS', 'annual': 'AEXMXUS'},
+    'Netherlands': {'daily': '', 'monthly': 'EXNEUS', 'annual': ''},
+    'New Zealand': {'daily': 'DEXUSNZ', 'monthly': 'EXUSNZ', 'annual': 'AEXUSNZ'},
+    'Norway': {'daily': 'DEXNOUS', 'monthly': 'EXNOUS', 'annual': 'AEXNOUS'},
+    'Portugal': {'daily': '', 'monthly': 'EXPOUS', 'annual': ''},
+    'Singapore': {'daily': 'DEXSIUS', 'monthly': 'EXSIUS', 'annual': 'AEXSIUS'},
+    'South Africa': {'daily': 'DEXSFUS', 'monthly': 'EXSFUS', 'annual': 'AEXSFUS'},
+    'South Korea': {'daily': 'DEXKOUS', 'monthly': 'EXKOUS', 'annual': 'AEXKOUS'},
+    'Spain': {'daily': '', 'monthly': 'EXSPUS', 'annual': ''},
+    'Sri Lanka': {'daily': '', 'monthly': 'EXSLUS', 'annual': ''},
+    'Sweden': {'daily': 'DEXSDUS', 'monthly': 'EXSDUS', 'annual': 'AEXSDUS'},
+    'Switzerland': {'daily': 'DEXSZUS', 'monthly': 'EXSZUS', 'annual': 'AEXSZUS'},
+    'Taiwan': {'daily': 'DEXTAUS', 'monthly': 'EXTAUS', 'annual': 'AEXTAUS'},
+    'Thailand': {'daily': 'DEXTHUS', 'monthly': 'EXTHUS', 'annual': 'AEXTHUS'},
+    'United Kingdom': {'daily': 'DEXUSUK', 'monthly': 'EXUSUK', 'annual': ''},
+    'Venezuela': {'daily': 'DEXVZUS', 'monthly': 'EXVZUS', 'annual': 'AEXVZUS'}
 }
 
 
@@ -48,16 +48,12 @@ def rename(package: PackageWrapper):
     package.pkg.descriptor['resources'][0]['path'] = 'data/daily.csv'
     package.pkg.descriptor['resources'][1]['name'] = 'exchange-rates-monthly'
     package.pkg.descriptor['resources'][1]['path'] = 'data/monthly.csv'
-    package.pkg.descriptor['resources'][2]['name'] = 'exchange-rates-yearly'
-    package.pkg.descriptor['resources'][2]['path'] = 'data/yearly.csv'
+    package.pkg.descriptor['resources'][2]['name'] = 'exchange-rates-annual'
+    package.pkg.descriptor['resources'][2]['path'] = 'data/annual.csv'
     yield package.pkg
     res_iter = iter(package)
-    first: ResourceWrapper = next(res_iter)
-    second: ResourceWrapper = next(res_iter)
-    third: ResourceWrapper = next(res_iter)
-    yield first.it
-    yield second.it
-    yield third.it
+    for res in res_iter:
+        yield res.it
     yield from package
 
 
@@ -80,12 +76,12 @@ def extract_exchange_rates(frequency):
                         value = ''
                     if country in inverse:
                         if value != '':
-                            value = str(round(1 / float(value), 4))
+                            value = str(1 / float(value))
 
                     yield {
                         'Date': date,
                         'Country': country,
-                        'Value': value
+                        'Exchange rate': value
                     }
                 if 'DATE' in line_str and 'VALUE' in line_str and header:
                     header = False
@@ -93,17 +89,89 @@ def extract_exchange_rates(frequency):
 
 exchange_rate_flow = Flow(
     add_metadata(
-        name="exchange-rates",
-        title="Exchange Rates",
+        name="us-euro-foreign-exchange-rate",
+        title="USA / EUR Foreign Exchange Rate since 1999",
         homepage='https://fred.stlouisfed.org',
-        licenses="PDDL-1.0",
+        sources=[
+            {
+                "name": "federal-reserve-bank-st-louis",
+                "title": "Federal Reserve Bank of St. Louis",
+                "path": "https://fred.stlouisfed.org/categories/158"
+            }
+        ],
+        licenses=[
+            {
+                "id": "odc-pddl",
+                "name": "public_domain_dedication_and_license",
+                "version": "1.0",
+                "url": "http://opendatacommons.org/licenses/pddl/1.0/"
+            }
+        ],
+        views=[
+            {
+                "name": "us-to-uk-foreign-exchange-rate",
+                "title": "USA / GBP Foreign Exchange Rate since 1971",
+                "resources": [
+                    {
+                        "name": "daily",
+                        "transform": [
+                            {
+                                "type": "filter",
+                                "expression": "data['Country'] === 'United Kingdom'"
+                            }
+                        ]
+                    }
+                ],
+                "specType": "simple",
+                "spec": {
+                    "type": "line",
+                    "group": "Date",
+                    "series": [
+                        "Exchange Rate"
+                    ]
+                }
+            },
+            {
+                "name": "us-euro-foreign-exchange-rate",
+                "title": "USA / EUR Foreign Exchange Rate since 1999",
+                "resources": [
+                    {
+                        "name": "daily",
+                        "transform": [
+                            {
+                                "type": "filter",
+                                "expression": "data['Country'] === 'Euro'"
+                            }
+                        ]
+                    }
+                ],
+                "specType": "simple",
+                "spec": {
+                    "type": "line",
+                    "group": "Date",
+                    "series": [
+                        "Exchange Rate"
+                    ]
+                }
+            }
+        ],
         version="0.2.0"
     ),
     extract_exchange_rates('daily'),
+    set_type('Date', type='date', format='any', description="Date in ISO format"),
+    set_type('Country', type='string', format='any', description="Name of a country"),
+    set_type('Exchange rate', type='number', format='any', description="Foreign Exchange Rate to USD. Only AUD, IEP, NZD, GBP and EUR to USD."),
+
     extract_exchange_rates('monthly'),
-    extract_exchange_rates('yearly'),
-    set_type('Date', type='date', format='any'),
-    set_type('Value', type='number', format='any'),
+    set_type('Date', type='date', format='any', description="Date in ISO format"),
+    set_type('Country', type='string', format='any', description="Name of a country"),
+    set_type('Exchange rate', type='number', format='any', description="Foreign Exchange Rate to USD. Only AUD, IEP, NZD, GBP and EUR to USD."),
+
+    extract_exchange_rates('annual'),
+    set_type('Date', type='date', format='any', description="Date in ISO format"),
+    set_type('Country', type='string', format='any', description="Name of a country"),
+    set_type('Exchange rate', type='number', format='any', description="Foreign Exchange Rate to USD. Only AUD, IEP, NZD, GBP and EUR to USD."),
+
     rename,
     validate(),
     dump_to_path(),
